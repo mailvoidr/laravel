78609740a2df36cfaa7b0ee076060e8d5d8d6ce6
codeWithMe <codewithhassan.404.not.found@gmail.com>
2026-06-29 15:28:51 +0100

    Prepare v0.1.0 for Packagist release

--- src/MailvoidrDefaults.php
+++ src/MailvoidrDefaults.php
@@ -4,7 +4,7 @@ namespace Mailvoidr\Laravel;
 
 final class MailvoidrDefaults
 {
-    public const BASE_URL = 'https://app.mailvoidr.com/api/v1';
+    public const BASE_URL = 'https://api.mailvoidr.com/api/v1';
 
     public const FROM = 'hello@mailvoidr.com';
 
